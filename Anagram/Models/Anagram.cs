f00e9af1fabb2526525c2bbe2a759962f1c7b103
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-17 21:50:57 -0800

    Add method to check pick the user input word out of list of words.

--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -1,14 +1,41 @@
 using System;
+using System.Collections.Generic;
 
-namespace Anagram.Modles
+namespace AnagramChecker.Models
 {
     public class Anagram
     {
-        public ClassConstructor()
+        public static List<string> GetAnagrams(string inputWord, string[] inputList)
         {
-            // Create constructor
+            List<string> anagrams = new List<string>();
+            for (int i = 0; i < inputList.Length; i ++)
+            {
+                bool isAnagram = Checker(inputWord, inputList[i]);
+                if (isAnagram) anagrams.Add(inputList[i]);
+            }
+
+            return anagrams;
         }
 
-        // Method
+        public static bool Checker(string word1, string word2)
+        {
+            char[] char1 = word1.ToLower() .ToCharArray();
+            char[] char2 = word2.ToLower() .ToCharArray();
+            
+            Array.Sort (char1);
+            Array.Sort (char2);
+            
+            string NewWord1 = string.Join("", char1);
+            string NewWord2 = string.Join("", char2);
+
+            if (NewWord1 == NewWord2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,14 +1,29 @@
 using System;
 using System.Collections.Generic;
 
-namespace Anagram.Models
+namespace AnagramChecker.Models
 {
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Please enter a word and we will return you a list of anagrams.");
 
+            Console.WriteLine("Welcome to the Anagram Tracker!");
+            Console.WriteLine("Please enter a word and we will check anagrams.");
+            string userInput = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Please enter a list of words, separated by commas: ");
+            string stringListWords = Console.ReadLine().ToLower();
+            string[] inputList = stringListWords.Split(',');
+
+            List<string> result = Anagram.GetAnagrams(userInput, inputList);
+
+            Console.WriteLine($"Your input word is '{userInput}'.");
+            Console.WriteLine($"The following words from your list are anagrams of {userInput}: ");
+            foreach(string word in result)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
